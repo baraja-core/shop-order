ba61458620762660f483078e56bfdad73fdf3431
Jan Barášek <janbarasek@gmail.com>
2021-12-20 15:50:50 +0100

    OrderStatusHistory: New entity for log order history.

---
+++ src/Entity/OrderStatusHistory.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Shop\Order\Entity;
+
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
+
+#[Entity]
+#[Table(name: 'shop__order_status_history')]
+class OrderStatusHistory
+{
+	#[ORM\Id]
+	#[ORM\Column(type: 'integer', unique: true, options: ['unsigned' => true])]
+	#[ORM\GeneratedValue]
+	protected int $id;
+
+	#[ORM\ManyToOne(targetEntity: Order::class)]
+	private Order $order;
+
+	#[ORM\ManyToOne(targetEntity: OrderStatus::class)]
+	private OrderStatus $status;
+
+	#[ORM\Column(type: 'datetime')]
+	private \DateTimeImmutable $insertedDate;
+
+
+	public function __construct(Order $order, OrderStatus $status)
+	{
+		$this->order = $order;
+		$this->status = $status;
+		$this->insertedDate = new \DateTimeImmutable;
+	}
+
+
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+
+	public function getOrder(): Order
+	{
+		return $this->order;
+	}
+
+
+	public function getStatus(): OrderStatus
+	{
+		return $this->status;
+	}
+
+
+	public function getInsertedDate(): \DateTimeImmutable
+	{
+		return $this->insertedDate;
+	}
+}
